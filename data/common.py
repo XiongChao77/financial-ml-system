4bfa9e1146b259da57e2dd5db532f62f2bb705a3
xiong chao <xc17671600218@gmail.com>
2025-09-24 14:15:28 +0400

    add macd ,all indicators have improved

--- data/common.py
+++ data/common.py
@@ -1,7 +1,9 @@
 #define model
-candlestick_num = 96
+candlestick_num = 120
 predict_num = 16
-change_rate = 0.002 # 0.2%
+change_rate = 0.004 # 0.2%
 label_decrease = 0
+# label_decrease_weak =1 
 label_ignore = 1
+# label_increase_weak = 3
 label_increase = 2
