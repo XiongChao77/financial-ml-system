d3b6555777995fa6f6cb07fa4ef425cd983367b5
XiongChao <xc17671600218@gmail.com>
2026-03-02 16:06:39 +0400

    update

--- experiment/reports_view.py
+++ experiment/reports_view.py
@@ -469,14 +469,14 @@ def main():
     # plot_heatmap(sorted_selected1,var1_key='predict_num',var2_key='predict_num',metric_key="l_sharpe",save_path=os.path.join(output_dir,f"l_sharpe_heatmap_combined.png"))
     # plot_heatmap(sorted_selected1,var1_key='predict_num',var2_key='predict_num',metric_key="l_calmar",save_path=os.path.join(output_dir,f"l_calmar_heatmap_combined.png"))
     # exit()
-    analyze_holdbar(sorted_selected1,target_key="predict_num",period ='long', metric_key="cagr")
+    analyze_holdbar(sorted_selected1,target_key="holdbar",period ='long', metric_key="cagr")
     if symbol == 'DOGEUSDT' and interval=='15m':
         l_results,unselected = filter_by_criteria(sorted_selected1, period ='long', cagr=0.6,rc_median = 0,rc_pos_ratio = 0.6,calmar = 1.9,daily_freq = 0.3,sharpe = 1)
     if symbol == 'ETHUSDT' and interval=='15m':
         l_results,unselected = filter_by_criteria(sorted_selected1, period ='long', cagr=0.2,rc_median = 0,rc_pos_ratio = 0.6,calmar = 1,daily_freq = 0.15,sharpe = 0.5)
 
     # sort_by_correlation_result = sort_by_correlation_diversity(l_results)
-    analyze_holdbar(l_results,target_key="predict_num",period ='long', metric_key="cagr")
+    analyze_holdbar(l_results,target_key="holdbar",period ='long', metric_key="cagr")
     # exit()
     # sorted_by_pos_ratio = sorted(
     #     l_results, 
